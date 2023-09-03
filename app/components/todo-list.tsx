5d9ae3dffe1ebd567f4dcde259b507ea307a0bbb
chemitaro <iwasi_44@hotmail.com>
2023-09-03 18:40:11 +0900

    Refactor todo list item class names to use double quotes

--- app/components/todo-list.tsx
+++ app/components/todo-list.tsx
@@ -7,7 +7,7 @@ export default async function TodoList() {
   const supabase = createServerComponentSupabaseClient<Database>({ headers, cookies })
   const { data: todos } = await supabase.from('todos').select().order('created_at', { ascending: true })
   return (
-    <ul className='my-6 mx-3'>
+    <ul className="my-6 mx-3">
       {todos?.map((todo) => (
         <TodoItem key={todo.id} {...todo} />
       ))}
